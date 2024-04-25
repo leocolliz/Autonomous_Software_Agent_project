c160fa503c3fa364501da36e31d05e5b472698e0
Edoardo Fiorentino <edo-fiore@hotmail.it>
2024-04-25 18:02:28 +0200

    feat: add intention_revision.js file

--- myAgent.js/config.js
+++ myAgent.js/config.js
@@ -3,7 +3,10 @@ const config = {
     host: "http://localhost:8080",
     // host: "https://deliveroojs.onrender.com",
 
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NWM4ODgzNzIxIiwibmFtZSI6ImxlbyIsImlhdCI6MTcxNDA0OTYyNn0.wBYsLgDVctR-ct2DyVzivCS8fHhX924Hp-aFko_gc5g'
+    // name: edo1
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjllZTdiYzBhMmQ1IiwibmFtZSI6ImVkbzEiLCJpYXQiOjE3MTM4MDE2OTV9.rLbUYMI4Ho0gMlJa-bwIRWyh_pHszHPFH8XDjVM6ZQo'
+
+    // name: leo
     // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMwNmI5MTZkZWYwIiwibmFtZSI6Im1hcmNvIiwiaWF0IjoxNjk2OTM5OTQyfQ.oILtKDtT-CjZxdnNYOEAB7F_zjstNzUVCxUWphx9Suw'
 
 }
